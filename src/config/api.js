4328b84ee90e0b31a4f4dfe38e46d374d0229d41
NicoelRepo <nicolasrepollo2000@hotmail.com>
2025-11-15 14:39:48 -0300

    Fix url back

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://5ng34s70gd.execute-api.us-east-1.amazonaws.com/';
 
 export const config = {
     API_BASE_URL,
